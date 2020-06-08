1b1318b7c8cb2919148cf921933723533077abad
eschi <eschilberg@outlook.de>
2020-06-08 12:49:05 +0200

    -fix Yawllmport

--- WoPeD-FileInterface/src/main/java/org/woped/file/yawl/model/LayoutAttributesFactsType.java
+++ WoPeD-FileInterface/src/main/java/org/woped/file/yawl/model/LayoutAttributesFactsType.java
@@ -8,16 +8,12 @@
 
 package org.woped.file.yawl.model;
 
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.annotation.*;
+
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-
-import jakarta.xml.bind.annotation.JAXBElement;
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlElementRef;
-import jakarta.xml.bind.annotation.XmlElementRefs;
-import jakarta.xml.bind.annotation.XmlType;
 
 
 /**
